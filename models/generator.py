5e9688d31e3ca172941d282e26db47c016b019de
root <choiseungho1019@gmail.com>
2023-11-13 05:32:22 +0000

    Change GAN->DCGAN, Add Gelu init weights

--- models/generator.py
+++ models/generator.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from transformers import PreTrainedModel
 from models.config import GeneratorConfig
 from typing import Optional, Literal
-
+from utils.train_utils import kaiming_normal_
 class GeneratorPreTrainedModel(PreTrainedModel):
     
     config_class = GeneratorConfig
@@ -12,9 +12,16 @@ class GeneratorPreTrainedModel(PreTrainedModel):
     supports_gradient_checkpointing = True
     
     def _init_weights(self, module):
-        if isinstance(module, nn.LayerNorm):
+        if isinstance(module, nn.Conv2d):
+            kaiming_normal_(module.weight, mode="fan_out", nonlinearity="gelu")
+            
+        elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
+            
+        elif isinstance(module, (nn.BatchNorm2d, nn.GroupNorm)):
+            nn.init.constant_(module.weight, 1)
+            nn.init.constant_(module.bias, 0)  
             
         elif isinstance(module, nn.Linear):
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
@@ -25,11 +32,9 @@ class Generator(GeneratorPreTrainedModel):
     
     def __init__(self, config:GeneratorConfig):
         super().__init__(config)
-        img_shape = (config.img_size ** 2) * config.img_channels
-        
         self.config = config
         self.encoder = GanEncoder(config)
-        self.projection = nn.Linear(128 * 2 ** config.num_layer, img_shape)
+        self.projection = nn.Conv2d(64, config.img_channels, kernel_size=3, stride=1, padding=1)
         self.act = nn.Tanh()
         
         self.post_init()
@@ -39,7 +44,7 @@ class Generator(GeneratorPreTrainedModel):
         image = self.projection(embeddings)
         image = self.act(image)
         
-        return image.view(input.size(0), self.config.img_channels, self.config.img_size, self.config.img_size)
+        return image
 
         
 class GanEncoder(nn.Module):
@@ -47,30 +52,44 @@ class GanEncoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.head = GanLayer(config.latent_dim, 128, config.activation, normalize=False)
-        self.layer = nn.ModuleList([GanLayer(128 * 2 ** i, 128 * 2 ** (i+1), config.activation) for i in range(config.num_layer)])
+        self.init_size = config.img_size // (2 ** config.num_layer)
+        self.init_dim = (64 * (2 ** config.num_layer))
+        self.head = nn.Linear(config.latent_dim, self.init_dim * self.init_size ** 2)
+        self.layer = nn.ModuleList([GanLayer(in_channels=64 * (2 ** (config.num_layer - i)), 
+                                             out_channels=64 * (2 ** (config.num_layer - i - 1)), 
+                                             activation = config.activation) for i in range(config.num_layer)])
         
     def forward(self, input: Optional[torch.Tensor]):
         output = self.head(input)
+        output = output.view(output.shape[0], self.init_dim, self.init_size, self.init_size)
         for i, layer_module in enumerate(self.layer):
             output = layer_module(output)
         return output
         
 class GanLayer(nn.Module):
     
-    def __init__(self, in_feat: int, out_feat: int, activation: str, normalize: bool=True):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int = 3, stride: int = 1, activation: str = "relu"
+    ):
         super().__init__()
-        self.in_feat = in_feat
-        self.out_feat = out_feat
-        self.activation = nn.functional.gelu if activation == "gelu" else (nn.ReLU if activation == "relu" else nn.LeakyReLU)
-        self.layer = nn.Linear(in_feat, out_feat)
-        self.norm = None
-        if normalize:
-            self.norm = nn.LayerNorm(out_feat, eps=1e-12)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.activation = nn.functional.gelu if activation == "gelu" else (nn.functional.relu if activation == "relu" else nn.functional.leaky_relu)
+        self.upsample = nn.Upsample(scale_factor=2)
+        self.block = self.conv_block(in_channels, out_channels, kernel_size, stride)
+        self.block2 = self.conv_block(out_channels, out_channels, kernel_size, stride)
+        
+    def conv_block(self, in_channels, out_channels, kernel_size, stride):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=1),
+            nn.BatchNorm2d(out_channels),
+        )
             
     def forward(self, input: Optional[torch.Tensor]):
-        output = self.layer(input)
-        if self.norm:
-            output = self.norm(output)
-        return self.activation(output)
+        _x = self.upsample(input)
+        _x = self.block(_x)
+        _x = self.block2(_x)
+        return self.activation(_x)
     
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,4 +1,102 @@
 import math
+import torch.nn as nn
+import torch
+import warnings
 
 def get_warmup_steps(warmup_steps: int, warmup_ratio:float, num_training_steps: int):
     return warmup_steps if warmup_steps > 0 else math.ceil(num_training_steps * warmup_ratio)
+
+def kaiming_normal_(
+    tensor: torch.Tensor, a: float = 0, mode: str = 'fan_in', nonlinearity: str = 'leaky_relu'
+):
+    r"""Fills the input `Tensor` with values according to the method
+    described in `Delving deep into rectifiers: Surpassing human-level
+    performance on ImageNet classification` - He, K. et al. (2015), using a
+    normal distribution. The resulting tensor will have values sampled from
+    :math:`\mathcal{N}(0, \text{std}^2)` where
+
+    .. math::
+        \text{std} = \frac{\text{gain}}{\sqrt{\text{fan\_mode}}}
+
+    Also known as He initialization.
+
+    Args:
+        tensor: an n-dimensional `torch.Tensor`
+        a: the negative slope of the rectifier used after this layer (only
+            used with ``'leaky_relu'``)
+        mode: either ``'fan_in'`` (default) or ``'fan_out'``. Choosing ``'fan_in'``
+            preserves the magnitude of the variance of the weights in the
+            forward pass. Choosing ``'fan_out'`` preserves the magnitudes in the
+            backwards pass.
+        nonlinearity: the non-linear function (`nn.functional` name),
+            recommended to use only with ``'relu'`` or ``'leaky_relu'`` (default).
+
+    Examples:
+        >>> w = torch.empty(3, 5)
+        >>> nn.init.kaiming_normal_(w, mode='fan_out', nonlinearity='relu')
+    """
+    if 0 in tensor.shape:
+        warnings.warn("Initializing zero-element tensors is a no-op")
+        return tensor
+    fan = nn.init._calculate_correct_fan(tensor, mode)
+    gain = calculate_gain(nonlinearity, a)
+    std = gain / math.sqrt(fan)
+    with torch.no_grad():
+        return tensor.normal_(0, std)
+
+def calculate_gain(nonlinearity, param=None):
+    r"""Return the recommended gain value for the given nonlinearity function.
+    The values are as follows:
+
+    ================= ====================================================
+    nonlinearity      gain
+    ================= ====================================================
+    Linear / Identity :math:`1`
+    Conv{1,2,3}D      :math:`1`
+    Sigmoid           :math:`1`
+    Tanh              :math:`\frac{5}{3}`
+    ReLU              :math:`\sqrt{2}`
+    Leaky Relu        :math:`\sqrt{\frac{2}{1 + \text{negative\_slope}^2}}`
+    SELU              :math:`\frac{3}{4}`
+    GELU              :math:`\sqrt{\frac{2}{\pi}}`
+    ================= ====================================================
+
+    .. warning::
+        In order to implement `Self-Normalizing Neural Networks`_ ,
+        you should use ``nonlinearity='linear'`` instead of ``nonlinearity='selu'``.
+        This gives the initial weights a variance of ``1 / N``,
+        which is necessary to induce a stable fixed point in the forward pass.
+        In contrast, the default gain for ``SELU`` sacrifices the normalisation
+        effect for more stable gradient flow in rectangular layers.
+
+    Args:
+        nonlinearity: the non-linear function (`nn.functional` name)
+        param: optional parameter for the non-linear function
+
+    Examples:
+        >>> gain = nn.init.calculate_gain('leaky_relu', 0.2)  # leaky_relu with negative_slope=0.2
+
+    .. _Self-Normalizing Neural Networks: https://papers.nips.cc/paper/2017/hash/5d44ee6f2c3f71b73125876103c8f6c4-Abstract.html
+    """
+    linear_fns = ['linear', 'conv1d', 'conv2d', 'conv3d', 'conv_transpose1d', 'conv_transpose2d', 'conv_transpose3d']
+    if nonlinearity in linear_fns or nonlinearity == 'sigmoid':
+        return 1
+    elif nonlinearity == 'tanh':
+        return 5.0 / 3
+    elif nonlinearity == 'relu':
+        return math.sqrt(2.0)
+    elif nonlinearity == 'leaky_relu':
+        if param is None:
+            negative_slope = 0.01
+        elif not isinstance(param, bool) and isinstance(param, int) or isinstance(param, float):
+            # True/False are instances of int, hence check above
+            negative_slope = param
+        else:
+            raise ValueError("negative_slope {} not a valid number".format(param))
+        return math.sqrt(2.0 / (1 + negative_slope ** 2))
+    elif nonlinearity == 'selu':
+        return 3.0 / 4  # Value found empirically (https://github.com/pytorch/pytorch/pull/50664)
+    elif nonlinearity == 'gelu':
+        return math.sqrt(2.0 / math.pi)
+    else:
+        raise ValueError("Unsupported nonlinearity {}".format(nonlinearity))
