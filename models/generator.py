8cd1428edb01d582320fb91a9469b0fcc9499e5b
root <choiseungho1019@gmail.com>
2023-11-15 07:53:07 +0000

    Add noise

--- models/generator.py
+++ models/generator.py
@@ -12,16 +12,17 @@ class GeneratorPreTrainedModel(PreTrainedModel):
     supports_gradient_checkpointing = True
     
     def _init_weights(self, module):
-        if isinstance(module, nn.Conv2d):
-            kaiming_normal_(module.weight, mode="fan_out", nonlinearity="gelu")
+        if isinstance(module, (nn.Conv2d, nn.ConvTranspose2d)):
+            # kaiming_normal_(module.weight, mode="fan_out", nonlinearity="gelu")
+            nn.init.normal_(module.weight.data, 0.0, 0.02)
             
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
             
         elif isinstance(module, (nn.BatchNorm2d, nn.GroupNorm)):
-            nn.init.constant_(module.weight, 1)
-            nn.init.constant_(module.bias, 0)  
+            nn.init.normal_(module.weight.data, 1.0, 0.02)
+            nn.init.constant_(module.bias, 0)
             
         elif isinstance(module, nn.Linear):
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
@@ -46,22 +47,22 @@ class Generator(GeneratorPreTrainedModel):
         
         return image
 
-        
+
 class GanEncoder(nn.Module):
-    
     def __init__(self, config):
         super().__init__()
         self.config = config
         self.init_size = config.img_size // (2 ** config.num_layer)
         self.init_dim = (64 * (2 ** config.num_layer))
-        self.head = nn.Linear(config.latent_dim, self.init_dim * self.init_size ** 2)
+        self.head = GanLayer(in_channels=config.latent_dim * 2, out_channels=64 * (2 ** (config.num_layer)), activation=config.activation, padding=0, kernel_size=self.init_size)
         self.layer = nn.ModuleList([GanLayer(in_channels=64 * (2 ** (config.num_layer - i)), 
-                                             out_channels=64 * (2 ** (config.num_layer - i - 1)), 
+                                             out_channels=64 * (2 ** (config.num_layer - (i+1))), 
                                              activation = config.activation) for i in range(config.num_layer)])
-        
+
     def forward(self, input: Optional[torch.Tensor]):
-        output = self.head(input)
-        output = output.view(output.shape[0], self.init_dim, self.init_size, self.init_size)
+        noise = torch.randn(input.size(0), input.size(1)).to(input.device)
+        reshaped_tensor = torch.cat((input, noise), dim=1).unsqueeze(-1).unsqueeze(-1)
+        output = self.head(reshaped_tensor)
         for i, layer_module in enumerate(self.layer):
             output = layer_module(output)
         return output
@@ -69,7 +70,7 @@ class GanEncoder(nn.Module):
 class GanLayer(nn.Module):
     
     def __init__(
-        self, in_channels: int, out_channels: int, kernel_size: int = 3, stride: int = 1, activation: str = "relu"
+        self, in_channels: int, out_channels: int, kernel_size: int = 4, stride: int = 2, activation: str = "relu", padding: int = 1
     ):
         super().__init__()
         self.in_channels = in_channels
@@ -77,19 +78,14 @@ class GanLayer(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.activation = nn.functional.gelu if activation == "gelu" else (nn.functional.relu if activation == "relu" else nn.functional.leaky_relu)
-        self.upsample = nn.Upsample(scale_factor=2)
-        self.block = self.conv_block(in_channels, out_channels, kernel_size, stride)
-        self.block2 = self.conv_block(out_channels, out_channels, kernel_size, stride)
+        self.block = self.conv_block(nn.ConvTranspose2d, in_channels, out_channels, kernel_size, stride, padding)
         
-    def conv_block(self, in_channels, out_channels, kernel_size, stride):
+    def conv_block(self, func, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=1),
+            func(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
             nn.BatchNorm2d(out_channels),
         )
             
     def forward(self, input: Optional[torch.Tensor]):
-        _x = self.upsample(input)
-        _x = self.block(_x)
-        _x = self.block2(_x)
+        _x = self.block(input)
         return self.activation(_x)
-    
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from typing import List, Optional, Literal
-from transformers import AutoImageProcessor, ResNetForImageClassification, AutoTokenizer
+from transformers import AutoImageProcessor, ResNetForImageClassification, AutoTokenizer, BertModel
 import torch
 from models.generator import Generator, GeneratorConfig
 from models.dpr import ImageTextRetrieval, ImageTextRetrievalConfig
@@ -75,7 +75,7 @@ class TrainArguments:
         default=False, metadata={"help": "Flag indicating whether to perform evaluation during training"}
     )
     train_batch_size: int = field(
-        default=64, metadata={"help": "Batch size for training"}
+        default=32, metadata={"help": "Batch size for training"}
     )
     eval_batch_size: int = field(
         default=8, metadata={"help": "Batch size for evaluation"}
@@ -102,7 +102,7 @@ class TrainArguments:
         default=0.0, metadata={"help": "Linear warmup over warmup_ratio fraction of total steps"}
     )
     save_term: int = field(
-        default=10, metadata={"help": "Specifies the interval (in some unit of time) at which data is saved or backed up. The value '10' represents the default save interval."}
+        default=50, metadata={"help": "Specifies the interval (in some unit of time) at which data is saved or backed up. The value '10' represents the default save interval."}
     )
     example_prompts: List[str] = field(
         default_factory=lambda: [
@@ -112,6 +112,25 @@ class TrainArguments:
         ],
         metadata={"help": "List of example prompts for generating images or descriptions"},
     )
+    
+def loss(v1: torch.Tensor, v2: torch.Tensor, positive_idx_per_question: list=None):
+    score = torch.matmul(v1, torch.transpose(v2, 0, 1))
+    if len(v1.size()) > 1:
+        q_num = v1.size(0)
+        score = score.view(q_num, -1)
+        
+    softmax_scores = F.log_softmax(score, -1)
+    if not positive_idx_per_question :
+        positive_idx_per_question = list(range(v1.size(0)))
+
+    loss = F.nll_loss(
+        softmax_scores,
+        torch.tensor(positive_idx_per_question).to(softmax_scores.device),
+        reduction="mean",
+    )
+    
+    return loss
+
 
 def train(args, generator, discriminator, tokenizer, processor, dataset):
     
@@ -134,48 +153,81 @@ def train(args, generator, discriminator, tokenizer, processor, dataset):
             return_tensors="pt"
         ).to(args.device)
     
-    
-    
-    
     # TODO if args.is_freeze:
-    if args.is_freeze:
-        for param in discriminator.parameters():
-            param.requires_grad = False
+    # if args.is_freeze:
+    #     for param in discriminator.parameters():
+    #         param.requires_grad = False
     
-    optimizer = optim_cls(generator.parameters(), lr=args.learning_rate)
+    optimizer = optim_cls(generator.parameters(), betas=(0.5, 0.999), lr=args.learning_rate)
+    optimizer_D = optim_cls(discriminator.parameters(), lr=args.learning_rate)
     num_training_steps = args.num_train_epochs * len(dataloader)
     scheduler = transformers.get_scheduler(args.scheduler_type, optimizer=optimizer, 
                                             num_warmup_steps=get_warmup_steps(args.warmup_steps, args.warmup_ratio, num_training_steps), 
                                             num_training_steps=num_training_steps)
     
+    scheduler_D = transformers.get_scheduler(args.scheduler_type, optimizer=optimizer_D, 
+                                            num_warmup_steps=get_warmup_steps(args.warmup_steps, args.warmup_ratio, num_training_steps), 
+                                            num_training_steps=num_training_steps)
+    
     for epoch in range(int(args.num_train_epochs)):
-        if epoch % args.save_term == 0:
+        with torch.no_grad():
             embs = discriminator.encode("text", **tokenized_text)
             outputs = generator(embs)
             for i, output in enumerate(outputs):
                 draw_examples(output, examples[i], args.output_dir, f"epoch_{epoch}_{i}")
-            generator.save_pretrained(os.path.join(args.output_dir, f"epoch_{epoch}"))
-        total_loss = 0.0
+            if epoch % args.save_term == 0:
+                generator.save_pretrained(os.path.join(args.output_dir, f"epoch_{epoch}"))
+        total_loss_g = 0.0
+        total_loss_d = 0.0
         
         for batch in tqdm(dataloader):
+            
+            for param in discriminator.parameters():
+                param.requires_grad = False
+            for param in generator.parameters():
+                param.requires_grad = True
+                
             optimizer.zero_grad()
+                
             text_embs = discriminator.encode("text", input_ids=batch["input_ids"].to(args.device), 
                                              token_type_ids=batch["token_type_ids"].to(args.device), 
                                              attention_mask=batch["attention_mask"].to(args.device))
             pixel_values = generator(text_embs)
-            
             image_embs = discriminator.encode("image", pixel_values=pixel_values)
+            origin_image_embs = discriminator.encode("image", pixel_values=batch["pixel_values"].to(args.device))
             
-            score = torch.matmul(image_embs, torch.transpose(text_embs, 0, 1))
-            sig = torch.sigmoid(torch.diagonal(score))
-            loss = F.binary_cross_entropy(sig, torch.tensor([1.0 for _ in range(image_embs.size(0))]).to(args.device))
-            total_loss += loss.item()
-            loss.backward()
+            v2 = torch.cat((image_embs.unsqueeze(1), origin_image_embs.unsqueeze(1)), dim=1)
+            v2 = v2.view(-1, 512)
+            label = [i for i in range(v2.size(0)) if i % 2 == 0]
+            loss_g = loss(text_embs, v2, label)
             
+            total_loss_g += loss_g.item()
+            loss_g.backward()
             optimizer.step()
             scheduler.step()
-        
-        print(f"{total_loss / len(dataloader)}")
+            
+            for param in discriminator.parameters():
+                param.requires_grad = True
+            for param in generator.parameters():
+                param.requires_grad = False
+
+            optimizer_D.zero_grad()
+            
+            text_embs = discriminator.encode("text", input_ids=batch["input_ids"].to(args.device), 
+                                    token_type_ids=batch["token_type_ids"].to(args.device), 
+                                    attention_mask=batch["attention_mask"].to(args.device))
+            image_embs = discriminator.encode("image", pixel_values=pixel_values.detach())
+            origin_image_embs = discriminator.encode("image", pixel_values=batch["pixel_values"].to(args.device))
+            v2 = torch.cat((origin_image_embs.unsqueeze(1), image_embs.unsqueeze(1)), dim=1)
+            v2 = v2.view(-1, 512)
+            label = [i for i in range(v2.size(0)) if i % 2 == 0]
+            loss_d = loss(text_embs, v2, label)
+            total_loss_d += loss_d.item()
+            loss_d.backward()
+            optimizer_D.step()
+            scheduler_D.step()
+            
+        print(f"{total_loss_g / len(dataloader)}, {total_loss_d / len(dataloader)}")
             
 
 if __name__ == "__main__":
@@ -184,10 +236,11 @@ if __name__ == "__main__":
     model_args, dpr_args, data_args, train_args = parser.parse_args_into_dataclasses()
     
     # generator = Generator.from_pretrained(**vars(model_args))
-    config = GeneratorConfig()
-    generator = Generator(config)
+    generator = Generator(GeneratorConfig(num_layer=5, activation="relu", img_size=224))
     
-    discriminator = ImageTextRetrieval.from_pretrained(dpr_args.dpr_model_name_or_path)
+    # discriminator = ImageTextRetrieval.from_pretrained(dpr_args.dpr_model_name_or_path)
+    discriminator = ImageTextRetrieval(ImageTextRetrievalConfig())
+    discriminator.text_encoder = BertModel.from_pretrained("bert-base-uncased")
     tokenizer = AutoTokenizer.from_pretrained(dpr_args.dpr_model_name_or_path)
     processor = AutoImageProcessor.from_pretrained(dpr_args.dpr_model_name_or_path)
     
